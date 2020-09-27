70f90b3685b59ab69184fea61e0aaca23ace3f95
ambrose mcduffy <ambrosemcduffy@gmail.com>
2020-09-27 21:55:24 +1000

    updated code to export dataset

--- gather_data.py
+++ gather_data.py
@@ -1,11 +1,11 @@
 import os
 import glob
 import shutil
+import pickle as pkl
 
 from PIL import Image
 import numpy as np
-import cv2
-import matplotlib.pyplot as plt
+import pandas as pd
 from collections import defaultdict
 
 from google_images_download import google_images_download
@@ -18,6 +18,19 @@ with open("data/names.txt", "r+") as f:
 
 
 def download_images(n_images=5, limit=20, download=False):
+    """
+    Parameters
+    ----------
+    n_images : TYPE, optional
+        DESCRIPTION. The default is 5.
+    limit : TYPE, optional
+        DESCRIPTION. The default is 20.
+    download : TYPE, optional
+        DESCRIPTION. The default is False.
+    Returns
+    -------
+    None
+    """
     cnt = 0
     if download is True:
         if os.path.exists("downloads/") is True:
@@ -35,15 +48,47 @@ def download_images(n_images=5, limit=20, download=False):
 
 
 def get_data():
-    d = defaultdict(list)
+    """
+    Returns
+    -------
+    data : Dictionary
+        This function returns data dict with image arrays
+    data_int : Dictionary
+        this function returns data_int that contains target arrays
+    """
+    # creating dictionary to store image files
+    data = defaultdict(list)
+    # obtaining the names of star trek character, and import images
     file_dir = glob.glob("downloads/*")
     for folder in file_dir:
         name = folder.split("/")[1].split("Star Trek")[0].rstrip()
         for file in glob.glob(folder+"/*"):
             img = Image.open(file)
-            d[name].append(np.array([np.array(img), name]))
-    return d
+            img = img.resize((244, 244))
+            img_arr = np.array(img)
+            if img_arr.shape == (244, 244, 3):
+                data[name].append(np.array(img_arr))
+    # obtaining target dummy variables
+    data_int = defaultdict(list)
+    for k, v in data.items():
+        for i in range(len(v)):
+            dummies = pd.get_dummies(list(data.keys()))[k].values
+            data_int[k].append(dummies)
+        data_int[k] = np.array(data_int[k])
+    return data, data_int
 
 
-#download_images(download=False)
-data = get_data()
+def export_dataset():
+    data, data_int = get_data()
+    x = []
+    y = []
+    for k, v in data.items():
+        x.append(np.array(data[k]))
+        y.append(np.array(data_int[k]))
+    x_train = np.vstack(x)
+    y_train = np.vstack(y)
+    with open("data/train.pkl", "wb") as f:
+        pkl.dump([x_train, y_train], f)
+
+
+export_dataset()
